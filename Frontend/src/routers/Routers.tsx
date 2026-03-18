f97ed4ea9470e72d87f5b9064fc19bac64517f71
Thanhhoai <nguyenthanhhoai230504@gmail.com>
2026-03-18 10:04:44 +0700

    fix/booking-schedule-navigation

--- Frontend/src/routers/Routers.tsx
+++ Frontend/src/routers/Routers.tsx
@@ -1,9 +1,24 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+<<<<<<< Updated upstream
 import { Login, SignUp } from "../pages";
 import MainLayout from "@/layout/MainLayout/MainLayout";
 import Home from "@/pages/Home";
 import CourtDetail from "@/pages/CourtDetail";
+=======
+import {
+  Login,
+  SignUp,
+  FavoriteCourts,
+  MyBookings,
+  AccountPage,
+} from "../pages";
+import MainLayout from "@/layout/MainLayout/MainLayout";
+import Home from "@/pages/Home";
+import CourtDetail from "@/pages/CourtDetail";
+import BookingSchedule from "@/pages/BookingSchedule";
+import Booking from "@/pages/Booking";
+>>>>>>> Stashed changes
 
 const Routers: React.FC = () => {
   return (
@@ -14,6 +29,13 @@ const Routers: React.FC = () => {
         <Route element={<MainLayout />}>
           <Route path="/" element={<Home />} />
           <Route path="/courts/:id" element={<CourtDetail />} />
+<<<<<<< Updated upstream
+=======
+          <Route path="/booking/:courtId" element={<Booking />} />
+          <Route path="/favorites" element={<FavoriteCourts />} />
+          <Route path="/my-bookings" element={<MyBookings />} />
+          <Route path="/account" element={<AccountPage />} />
+>>>>>>> Stashed changes
         </Route>
       </Routes>
     </BrowserRouter>
