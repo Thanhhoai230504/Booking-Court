3da25d5dbd9ca3bf5c69eba5ea6c2519b445e486
PhucToan-hue <phuctoan2709@gmail.com>
2026-04-18 16:30:17 +0700

    Update page exports

--- Frontend/src/pages/index.ts
+++ Frontend/src/pages/index.ts
@@ -1,5 +1,21 @@
+export { default as Home } from './Home';
 export { default as Login } from './Login';
 export { default as SignUp } from './SignUp';
-export { default as FavoriteCourts } from './FavoriteCourts';
+export { default as CourtDetail } from './CourtDetail';
+export { default as Booking } from './Booking';
+export { default as BookingSchedule } from './BookingSchedule';
 export { default as MyBookings } from './MyBookings';
 export { default as AccountPage } from './AccountPage';
+export { default as FavoriteCourts } from './FavoriteCourts';
+
+// Admin pages
+export { default as AdminDashboard } from './admin/AdminDashboard';
+export { default as AdminCourts } from './admin/AdminCourts';
+export { default as AdminDrinks } from './admin/AdminDrinks';
+export { default as AdminRevenue } from './admin/AdminRevenue';
+
+// Admin Management pages (admin only)
+export { default as AdminManagementDashboard } from './admin-management/AdminManagementDashboard';
+export { default as ManageOwners } from './admin-management/ManageOwners';
+export { default as ManageCustomers } from './admin-management/ManageCustomers';
+export { default as SystemRevenue } from './admin-management/SystemRevenue';
